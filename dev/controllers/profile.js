6655aa380fc656a5bd539a4dabfcaa9fa46da5b8
angelraver <angelraver@gmail.com>
2017-05-27 19:56:52 -0300

    highcharts successfully implemented

--- dev/controllers/profile.js
+++ dev/controllers/profile.js
@@ -1,7 +1,34 @@
+import Highcharts from 'highcharts';
 class ProfileController {
-
   constructor($scope) {
     'ngInject';
+
+
+
+    var myChart = Highcharts.chart('container', {
+      chart: {
+        type: 'bar'
+      },
+      title: {
+        text: 'Fruit Consumption'
+      },
+      xAxis: {
+        categories: ['Apples', 'Bananas', 'Oranges']
+      },
+      yAxis: {
+        title: {
+          text: 'Fruit eaten'
+        }
+      },
+      series: [{
+        name: 'Jane',
+        data: [1, 0, 4]
+        }, {
+        name: 'John',
+        data: [5, 7, 3]
+        }]
+    });
+
 
   }
 }
